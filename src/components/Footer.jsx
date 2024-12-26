0e47e7c1a9c881a6392b73ea0551435012485ddf
nomanahmeda789 <nomanahmeda789@gmail.com>
2024-12-26 19:34:52 +0500

    added Socials links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,24 +30,24 @@ const sitemap = [
 const socials = [
   {
     label: "GitHub",
-    href: "https://www.github.com/codewithsadee-org",
+    href: "https://github.com/Nomanahmeda789",
   },
   {
     label: "LinkedIn",
-    href: "https://www.linkedin.com/in/codewithsadee",
+    href: "https://www.linkedin.com/in/noman-ahmed-971796148/",
   },
-  {
-    label: "Twitter X",
-    href: "https://x.com/codewithsadee_",
-  },
-  {
-    label: "Instagram",
-    href: "https://www.instagram.com/codewithsadee",
-  },
-  {
-    label: "CodePen",
-    href: "https://codepen.io/codewithsadee",
-  },
+  // {
+  //   label: "Twitter X",
+  //   href: "https://x.com/codewithsadee_",
+  // },
+  // {
+  //   label: "Instagram",
+  //   href: "https://www.instagram.com/codewithsadee",
+  // },
+  // {
+  //   label: "CodePen",
+  //   href: "https://codepen.io/codewithsadee",
+  // },
 ];
 const Footer = () => {
   return (
